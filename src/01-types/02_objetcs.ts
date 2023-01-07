fe79b87b82356a83e0407fc61af206b8d79a00bc
mohnoor94 <moh.noor94@gmail.com>
2023-01-07 18:01:16 +0300

    Object Type

---
+++ src/01-types/02_objetcs.ts
@@ -0,0 +1,12 @@
+
+// An object is a collection of properties, and a property is an association between a name (or key) and a value.
+const person = {
+    name: 'John', // property `name` with a value of type `string`
+    age: 30 // property `age` with a value of type `number`
+};
+
+console.log(person.name); // John
+console.log(person.age); // 30
+console.log(person); // { name: 'John', age: 30 }
+
+// the type of the above object is: { name: string; age: number; }
